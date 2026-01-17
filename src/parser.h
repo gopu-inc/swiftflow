554e9f4ba315d76c747fec46f3055dd72d1b9037
Mauricio-100 <ceoseshell@gmail.com>
2026-01-17 14:27:11 +0100

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -1,16 +1,36 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "common.h"
+#include "lexer.h"
 #include "value.h"
 
+// Types de nœuds AST
+typedef enum {
+    NODE_PROGRAM, NODE_BLOCK, NODE_VAR_DECL,
+    NODE_FUNCTION, NODE_IF, NODE_WHILE, NODE_RETURN,
+    NODE_BINARY, NODE_UNARY, NODE_ASSIGN,
+    NODE_LITERAL, NODE_IDENTIFIER,
+    NODE_CALL, NODE_EXPR_STMT
+} NodeType;
+
 typedef struct ASTNode {
-    int type;  // Pour l'instant, simple placeholder
-    void* data;
+    NodeType type;
+    Token token;
+    struct ASTNode* left;
+    struct ASTNode* right;
+    struct ASTNode** children;
+    int child_count;
+    ValueType inferred_type;
 } ASTNode;
 
-// Fonctions de parsing (stubs pour l'instant)
+// Fonctions du parser
 ASTNode* parse(const char* source);
-Value eval(ASTNode* node, Environment* env);
+ASTNode* new_node(NodeType type);
+ASTNode* expression();
+ASTNode* statement();
+ASTNode* declaration();
+
+// Variables globales
+extern ASTNode* current_function;
 
 #endif
