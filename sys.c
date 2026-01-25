e6361b03cea13f10f35fb295526228aa57ef19bf
Mauricio-100 <ceoseshell@gmail.com>
2026-01-25 13:14:16 +0100

    Update sys.c

--- sys.c
+++ sys.c
@@ -11,49 +11,29 @@ static char** g_argv;
 void init_sys_module(int argc, char** argv) {
     g_argc = argc;
     g_argv = argv;
-    printf("%s[SYS MODULE]%s Initialized\n", COLOR_CYAN, COLOR_RESET);
+    printf("%s[SYS MODULE]%s Initialized (%d args)\n", COLOR_CYAN, COLOR_RESET, argc);
 }
 
-// sys.exec("cmd") -> int (code de retour)
-void sys_exec(ASTNode* node) {
-    if (!node->left) return;
-    
-    // Extraction manuelle car swf.c n'est pas inclus ici
-    char* cmd = NULL;
-    if (node->left->type == NODE_STRING) cmd = node->left->data.str_val;
-    
-    // Note: Dans une version finale, utilisez une fonction helper partagée pour évaluer les variables
-    if (cmd) {
-        int res = system(cmd);
-        node->data.int_val = res; // Retourne le code de sortie
-    }
-}
-
-// sys.argv(index) -> string
-void sys_argv(ASTNode* node) {
-    if (!node->left) return;
-    int idx = 0;
-    if (node->left->type == NODE_INT) idx = (int)node->left->data.int_val;
-    
-    // On décale de 1 pour ignorer le nom du programme swift lui-même si besoin,
-    // ou on garde tel quel. Ici on accède aux arguments passés au script.
-    // Hack: on suppose que argv[2] est le premier arg du script si lancé via ./swift script.swf arg1
-    
-    // Ajustement selon comment tu lances le script
-    // ./swift zarch.swf install math
-    // argv[0]=./swift, argv[1]=zarch.swf, argv[2]=install, argv[3]=math
-    
-    int real_idx = idx + 2; 
+// Récupère l'argument à l'index donné.
+// Note : Index 0 correspond au premier argument APRES le nom du script
+// Usage : ./swift zarch.swf install (install = index 0)
+char* sys_get_argv(int index) {
+    // Offset de 2 : [0]=./swift, [1]=zarch.swf, [2]=Arg0
+    int real_idx = index + 2;
     
     if (real_idx < g_argc) {
-        node->data.str_val = strdup(g_argv[real_idx]); // Hack temporaire de retour
-    } else {
-        node->data.str_val = NULL;
+        return g_argv[real_idx]; // Retourne le pointeur direct (lecture seule)
     }
+    return NULL;
 }
 
-void sys_exit(ASTNode* node) {
-    int code = 0;
-    if (node->left) code = (int)node->left->data.int_val;
-    exit(code);
+// Exécute une commande shell et retourne le code de sortie
+int sys_exec_int(const char* cmd) {
+    if (!cmd) return -1;
+    printf("%s[SYS EXEC]%s %s\n", COLOR_YELLOW, COLOR_RESET, cmd);
+    int res = system(cmd);
+    
+    // WEXITSTATUS pour avoir le vrai code de retour Linux (0-255)
+    if (res == -1) return -1;
+    return (res >> 8) & 0xFF; 
 }
