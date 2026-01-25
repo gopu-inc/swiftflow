854f028cd168e9d221f63e9e0ba6cd335cb93b3f
Mauricio-100 <ceoseshell@gmail.com>
2026-01-25 13:12:49 +0100

    Update sys.h

--- sys.h
+++ sys.h
@@ -1,8 +1,8 @@
 #ifndef SYS_H
 #define SYS_H
-#include "common.h"
+
 void init_sys_module(int argc, char** argv);
-void sys_exec(ASTNode* node);   // ex: sys.exec("ls -la")
-void sys_argv(ASTNode* node);   // ex: sys.argv(1) -> retourne l'argument 1
-void sys_exit(ASTNode* node);   // ex: sys.exit(0)
+char* sys_get_argv(int index);
+int sys_exec_int(const char* cmd);
+
 #endif
