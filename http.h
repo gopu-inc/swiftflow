e6ba261464971c8b4c05e3402da242231efc261a
Mauricio-100 <ceoseshell@gmail.com>
2026-01-25 12:49:59 +0100

    Create http.h

---
+++ http.h
@@ -0,0 +1,12 @@
+// http.h
+#ifndef HTTP_H
+#define HTTP_H
+
+#include "common.h"
+
+void init_http_module(void);
+char* http_get(const char* url);
+char* http_post(const char* url, const char* json_data);
+char* http_download(const char* url, const char* output_filename);
+
+#endif
