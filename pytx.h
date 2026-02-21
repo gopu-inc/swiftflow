16238d10dcd98bb554d282710f44b9e364c3ea8e
Mauricio-100 <ceoseshell@gmail.com>
2026-02-21 16:48:27 +0100

    Update pytx.h

--- pytx.h
+++ pytx.h
@@ -3,51 +3,18 @@
 #define PYTX_H
 
 #include <python3.9/Python.h>
-#include "common.h"
 
+// Structure pour stocker un module Python importé
 typedef struct {
-    PyObject* py_module;      // Module Python importé
-    char* module_name;         // Nom du module
-    PyObject* py_dict;         // Dictionnaire des fonctions/méthodes
-    bool is_loaded;            
+    PyObject* py_module;
+    char* module_name;
+    PyObject* py_dict;
+    bool is_loaded;
 } PyTxModule;
 
-// Cache des modules Python importés
-static PyTxModule py_modules[100];
-static int py_module_count = 0;
+// Fonctions du module pytx
+void init_pytx(void);
+PyTxModule* pytx_import(const char* module_name);
+void pytx_cleanup(void);
 
-// Initialisation de l'interpréteur Python
-void init_pytx(void) {
-    Py_Initialize();
-    // Ajouter le chemin des modules système
-    PyRun_SimpleString("import sys; sys.path.append('.')");
-}
-
-// Import d'un module Python
-PyTxModule* pytx_import(const char* module_name) {
-    // Vérifier le cache
-    for (int i = 0; i < py_module_count; i++) {
-        if (strcmp(py_modules[i].module_name, module_name) == 0) {
-            return &py_modules[i];
-        }
-    }
-    
-    // Importer le module Python
-    PyObject* py_name = PyUnicode_FromString(module_name);
-    PyObject* py_module = PyImport_Import(py_name);
-    Py_DECREF(py_name);
-    
-    if (!py_module) {
-        PyErr_Print();
-        return NULL;
-    }
-    
-    // Stocker dans le cache
-    PyTxModule* mod = &py_modules[py_module_count++];
-    mod->py_module = py_module;
-    mod->module_name = str_copy(module_name);
-    mod->py_dict = PyModule_GetDict(py_module);
-    mod->is_loaded = true;
-    
-    return mod;
-}
+#endif
