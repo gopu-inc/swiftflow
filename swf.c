7ecb543866f4f0944f2d2db072d8f8e2fcb95e80
Mauricio-100 <ceoseshell@gmail.com>
2026-02-26 09:33:12 +0100

    Update swf.c

--- swf.c
+++ swf.c
@@ -2969,6 +2969,7 @@ int main(int argc, char* argv[]) {
     init_io_module();
     init_sys_module(argc, argv);
     init_http_module();
+    init_sys_module(argc, argv);
     // Handle command line arguments
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0) {
